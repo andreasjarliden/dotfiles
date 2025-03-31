f86b4bc225e833acb698c972a25934b7c3f8f54e
seeriand <andreas.eriksson@elekta.com>
2025-03-31 20:54:11 +0200

    Wezterm colorscheme and fontsize

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,9 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Builtin Solarized Dark'
+config.font_size = 15.0
 
 -- and finally, return the configuraiton to wezterm
 return config
