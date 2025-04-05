a6ff2ba54b13af4a2f0573bbf5ac9ae291f3e79d
seeriand <andreas.eriksson@elekta.com>
2025-04-05 18:47:00 +0200

    Add nvim-treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,7 +37,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  { "nvim-treesitter/nvim-treesitter", name = "nvim-treesitter" }
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
@@ -48,3 +49,5 @@ vim.cmd.colorscheme "catppuccin"
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
