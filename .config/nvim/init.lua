25b06330f7c3e131ab2fbb2d213fdb75b8035fa0
Andreas Jarliden <aeriksson@gmail.com>
2025-04-09 21:04:11 +0200

    Modularized nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,3 @@
--- Tabs
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Cursor style
-vim.cmd("set cursorline")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-
--- Use k as leader
-vim.g.mapleader = "k"
-
--- Use kk as <Esc> in insert mode
-vim.cmd("inoremap kk <Esc>")
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -32,22 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { "nvim-treesitter/nvim-treesitter", name = "nvim-treesitter" }
-}
-local opts = {}
-require("lazy").setup(plugins, opts)
+require("vim-options")
+require("lazy").setup("plugins")
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,18 @@
+-- Tabs
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Cursor style
+vim.cmd("set cursorline")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+-- Use space as leader
+vim.g.mapleader = " "
+
+-- Use kk as <Esc> in insert mode
+vim.cmd("inoremap kk <Esc>")
+
+
