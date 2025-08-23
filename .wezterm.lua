78ea133ac906a1e03f09c77e98ff9c065c692bd4
seeriand <andreas.eriksson@elekta.com>
2025-08-23 11:49:41 +0200

    Use same colortheme in wezterm as in neovim

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- config.color_scheme = 'AdventureTime'
 -- config.color_scheme = 'Builtin Solarized Dark'
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = 'Catppuccin Mocha'
 
 config.font_size = 14.0
 
