e6155da4cdd6ea449d801ad9a0165d9f757e8c80
seeriand <andreas.eriksson@elekta.com>
2025-09-27 14:55:54 +0200

    Improvements to postInstallTumbler.sh
    - Ensure it is run using sudo.
    - Install all rpms in one invocation.

--- postInstall/postInstallTumbler.sh
+++ postInstall/postInstallTumbler.sh
@@ -1,16 +1,26 @@
+if [[ $EUID -ne 0 ]]; then
+	echo Run using sudo
+	exit -1
+fi
+
 hostnamectl hostname tumbler
 
 # mdns
 firewall-cmd --permanent --add-service=mdns
 firewall-cmd --reload
 
-zypper install wezterm
-zypper install git
-zypper install neovim
-zypper install neofetch
-zypper install gnome-shell-extension-user-theme
-zypper install btop htop
-zypper install stress-ng
-zypper install rpm-build
-zypper install rpmdevtools
-zypper install plocate
+packages="wezterm
+git
+neovim
+neofetch
+gnome-shell-extension-user-theme
+btop htop
+stress-ng
+rpm-build
+rpmdevtools
+plocate
+tmux
+cmake
+flex"
+
+zypper install $packages
