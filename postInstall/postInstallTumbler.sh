2d1f14ea0175fd83e2cac5c0921b4ee872a71cf7
seeriand <andreas.eriksson@elekta.com>
2026-04-03 12:05:38 +0200

    Minor fixes

--- postInstall/postInstallTumbler.sh
+++ postInstall/postInstallTumbler.sh
@@ -5,6 +5,10 @@ fi
 
 hostnamectl hostname tumbler
 
+# Keyboard. Ensure no problems with dead keys, e.g. ~
+# This was at least needed for lowball and the gnome Console
+sudo localectl set-x11-keymap se nodeadkeys
+
 # mdns
 firewall-cmd --permanent --add-service=mdns
 firewall-cmd --reload
